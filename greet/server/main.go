eb13ba034dfea18846be6913d25bc1df6eba89e4
Cristian Ruiz <cristianrb235@gmail.com>
2022-11-27 19:38:37 +0100

    Init project and add protos for greet

---
+++ greet/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "cristianrb/greet/proto"
+)
+
+var addr string = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+	s := grpc.NewServer()
+	if err = s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
