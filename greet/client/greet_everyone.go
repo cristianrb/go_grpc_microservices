5c3add49d5911988b34b7b73be094b4f98622f23
Cristian Ruiz <cristianrb235@gmail.com>
2022-11-30 19:59:11 +0100

    Add greet everyone

---
+++ greet/client/greet_everyone.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	pb "cristianrb/greet/proto"
+	"io"
+	"log"
+	"time"
+)
+
+func doGreetEveryone(c pb.GreetServiceClient) {
+	log.Println("doGreetEveryone was invoked")
+
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v\n", err)
+	}
+
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Cristian"},
+		{FirstName: "Marie"},
+		{FirstName: "Blas"},
+	}
+
+	waitc := make(chan struct{})
+
+	go func() {
+		for _, req := range reqs {
+			log.Printf("Sending request: %v\n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Printf("Error while receiving: %v\n", err)
+				break
+			}
+
+			log.Printf("Received: %v\n", res.Result)
+		}
+
+		close(waitc)
+	}()
+
+	<-waitc
+}
