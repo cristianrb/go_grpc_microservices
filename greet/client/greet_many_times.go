31ad6c52bbc5f1e559a43ff6ac848220aaf014b6
Cristian Ruiz <cristianrb235@gmail.com>
2022-11-30 07:42:08 +0100

    Add greet many times stream

---
+++ greet/client/greet_many_times.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	pb "cristianrb/greet/proto"
+	"io"
+	"log"
+)
+
+func doGreetManyTimes(c pb.GreetServiceClient) {
+	log.Println("doGreetManyTimes was invoked")
+
+	req := &pb.GreetRequest{
+		FirstName: "Cristian",
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Error while calling GreetManyTimes: %v\n", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Fatalf("Error while reading the stream: %v\n", err)
+		}
+
+		log.Printf("GreetManyTimes: %s", msg.Result)
+	}
+}
