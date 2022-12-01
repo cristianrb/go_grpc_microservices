aef335aecbe48011de6ad04ab4a0911718324750
Cristian Ruiz <cristianrb235@gmail.com>
2022-12-01 07:52:53 +0100

    Add ssl security

--- greet/client/main.go
+++ greet/client/main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"log"
-	"time"
-
 	pb "cristianrb/greet/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"log"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	tls := true // change that to false if needed
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile := "ssl/ca.crt"
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
 	}
 	defer conn.Close()
 
 	c := pb.NewGreetServiceClient(conn)
-	//doGreet(c)
+	doGreet(c)
 	//doGreetManyTimes(c)
 	//doLongGreet(c)
 	//doGreetEveryone(c)
-	doGreetWithDeadline(c, 5*time.Second)
-	doGreetWithDeadline(c, 1*time.Second)
+	//doGreetWithDeadline(c, 5*time.Second)
+	//doGreetWithDeadline(c, 1*time.Second)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"log"
 	"net"
 
@@ -21,7 +22,21 @@ func main() {
 	}
 
 	log.Printf("Listening on %s\n", addr)
-	s := grpc.NewServer()
+
+	opts := []grpc.ServerOption{}
+	tls := true // change that to false if needed
+	if tls {
+		certFile := "ssl/server.crt"
+		keyFile := "ssl/server.pem"
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed loading certificates: %v\n", err)
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v", err)
