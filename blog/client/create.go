59583ffeda9752759fe6c34dc782c713bb685428
Cristian Ruiz <cristianrb235@gmail.com>
2022-12-01 08:33:34 +0100

    Add blog create endpoint

---
+++ blog/client/create.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	pb "cristianrb/blog/proto"
+	"log"
+)
+
+func createBlog(c pb.BlogServiceClient) string {
+	log.Println("---createBlog was invoked---")
+
+	blog := &pb.Blog{
+		AuthorId: "Cristian",
+		Title:    "My first blog",
+		Content:  "Content of the first blog",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+	if err != nil {
+		log.Fatalf("Unexpected error: %v\n", err)
+	}
+
+	log.Printf("Blog has been created: %s\n", res.Id)
+	return res.Id
+}
